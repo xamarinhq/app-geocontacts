d20adb360dcda36f84db039af2d5a19c8b4d3570
James Montemagno <james.montemagno@gmail.com>
2018-02-06 09:56:54 -0800

    Initial code commit

---
+++ src/AwesomeContacts/AwesomeContacts/ViewModel/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmHelpers;
+
+namespace AwesomeContacts.ViewModel
+{
+    public class ViewModelBase : BaseViewModel
+    {
+    }
+}
