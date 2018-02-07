f55086ce7b0127498d4568ed0d7057471248e429
James Montemagno <james.montemagno@gmail.com>
2018-02-07 10:26:12 -0800

    Add post clones

---
+++ src/AwesomeContacts/AwesomeContacts.Android/mobile-center-post-clone.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Variables:"
+printenv
+
+echo "Arguments for updating:"
+echo " - ACID: $AC_ANDROID"
+
+# Updating manifest
+sed -i '' "s/AC_ANDROID/$AC_ANDROID/g" $BUILD_REPOSITORY_LOCALPATH/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
+
+cat $BUILD_REPOSITORY_LOCALPATH/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
+
+echo "Manifest updated!"
