1eed7ddea1899e255ca761d771ae09719588be79
James Montemagno <james.montemagno@gmail.com>
2018-02-20 20:50:59 -0800

    cleanup scripts

--- src/AwesomeContacts/AwesomeContacts.Android/mobile-center-post-clone.sh
+++ src/AwesomeContacts/AwesomeContacts.Android/mobile-center-post-clone.sh
@@ -7,8 +7,8 @@ echo "Arguments for updating:"
 echo " - ACID: $AC_ANDROID"
 
 # Updating manifest
-sed -i '' "s/AC_ANDROID/$AC_ANDROID/g" $BUILD_REPOSITORY_LOCALPATH/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
+sed -i '' "s/AC_ANDROID/$AC_ANDROID/g" $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
 
-cat $BUILD_REPOSITORY_LOCALPATH/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
+cat $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
 
 echo "Manifest updated!"
--- src/AwesomeContacts/AwesomeContacts.iOS/mobile-center-post-clone.sh
+++ src/AwesomeContacts/AwesomeContacts.iOS/mobile-center-post-clone.sh
@@ -7,8 +7,8 @@ echo "Arguments for updating:"
 echo " - ACID: $AC_IOS"
 
 # Updating manifest
-sed -i '' "s/AC_IOS/$AC_IOS/g" $BUILD_REPOSITORY_LOCALPATH/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
+sed -i '' "s/AC_IOS/$AC_IOS/g" $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
 
-cat $BUILD_REPOSITORY_LOCALPATH/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
+cat $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
 
 echo "Manifest updated!"
