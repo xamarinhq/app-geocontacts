273a4c27d40354a1e456f846630deca72dc1d21a
James Montemagno <james.montemagno@gmail.com>
2018-02-23 14:19:57 -0800

    Update mobile-center-post-clone.sh

--- src/mobile-center-post-clone.sh
+++ src/mobile-center-post-clone.sh
@@ -5,11 +5,11 @@ printenv
 
 echo "Arguments for updating:"
 echo " - ACID: $AC_IOS"
-echo " - ACSECRET: $AC_SECRET"
+echo " - ACSECRET: $APP_SECRET"
 
 # Updating manifest
 sed -i '' "s/AC_IOS/$AC_IOS/g" $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
-sed -i '' "s/AC_SECRET/$AC_SECRET/g" $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts.iOS/Info.plist
+sed -i '' "s/APP_SECRET/$ACPP_SECRET/g" $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts.iOS/Info.plist
 
 cat $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
 cat $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts.iOS/Info.plist
