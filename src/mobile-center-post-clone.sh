1be0ff13250bd5c2d172820b5d75e38da5ea15ea
James Montemagno <james.montemagno@gmail.com>
2018-02-27 16:52:09 -0800

    Update mobile-center-post-clone.sh

--- src/mobile-center-post-clone.sh
+++ src/mobile-center-post-clone.sh
@@ -11,6 +11,10 @@ echo " - ACSECRET: $APP_SECRET"
 sed -i '' "s/AC_IOS/$AC_IOS/g" $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
 sed -i '' "s/APP_SECRET/$APP_SECRET/g" $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts.iOS/Info.plist
 
+if [ "$APPCENTER_BRANCH" == "appstore" ]; then
+  /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier com.microsoft.advocates" "$BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts.iOS/Info.plist"
+fi
+
 cat $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts/Helpers/CommonConstants.cs
 cat $BUILD_REPOSITORY_LOCALPATH/src/AwesomeContacts/AwesomeContacts.iOS/Info.plist
 
