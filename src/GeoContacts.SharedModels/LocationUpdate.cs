275f652ee0d107ef0fd4279bc7b71a0531e4a72e
Matt Soucoup <msoucoup@codemilltech.com>
2018-03-12 11:01:39 -0500

    Adding delete timer (#40)

--- src/GeoContacts.SharedModels/LocationUpdate.cs
+++ src/GeoContacts.SharedModels/LocationUpdate.cs
@@ -11,9 +11,12 @@ namespace GeoContacts.SharedModels
         [JsonProperty("position")]
         public Point Position { get; set; }
 
+        [JsonProperty("id")]
+        public string Id { get; set; }
         public string Country { get; set; }
         public string Town { get; set; }
         public string State { get; set; }
+        [JsonProperty("userPrincipalName")]
         public string UserPrincipalName { get; set; }
         public DateTimeOffset InsertTime { get; set; }
     }
